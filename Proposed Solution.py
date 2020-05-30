9b64abf40a605a8121dc587a8bde47cf69e9498f
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-05-30 10:30:23 +0000

    bump 0.0.2

--- Proposed Solution.py
+++ Proposed Solution.py
@@ -1,4 +1,4 @@
 ##################################################
 ## Deeplabv3 Silhouette Extractor
 ##################################################
-## Takes video file as i
+## Takes video file as input, 
