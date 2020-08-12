ASoC: wm2000: Fix wrong value references for boolean kctl

Proposed Solution.py
@@ -21,4 +21,5 @@ model = torch.hub.load('pytorch/vision:v0.6.0', 'deeplabv3_resnet101', pretraine
 people_class = 15
 
 # Evaluate model
-model.ev
+model.eval()
+prin