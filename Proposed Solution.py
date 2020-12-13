f3a9401bdfebd89f1c9b4d4de41332d6fa6a6bc6
qubilyan <151318276+qubilyan@users.noreply.github.com>
2020-12-13 22:30:56 +0000

    Kick from realglobe-Inc/sugo-hub

--- Proposed Solution.py
+++ Proposed Solution.py
@@ -46,4 +46,5 @@ def makeSegMask(img):
     # Create mini-batch to be used by the model
 	input_batch = input_tensor.unsqueeze(0)
 
-    # Use GPU if supported, for better performanc
+    # Use GPU if supported, for better performance
+	if torch
