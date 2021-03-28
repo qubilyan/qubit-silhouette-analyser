fd80e5e35fd5c2024fbe502dada201584826e902
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-03-28 22:34:09 +0000

    Added app icons

--- Proposed Solution.py
+++ Proposed Solution.py
@@ -66,4 +66,4 @@ def makeSegMask(img):
 		people = F.conv2d(people, blur, stride=1, padding=1)
 
 	# Activation function to combine masks - F.hardtanh(a * b)
-	combined_mask = F.relu(F.hardtanh(a * (people.squeeze().pow(1.
+	combined_mask = F.relu(F.hardtanh(a * (people.squeeze().pow(1.5))
