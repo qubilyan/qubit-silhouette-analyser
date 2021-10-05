9add6b33649a0650da92b99721a2d751b939cbd9
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-10-05 22:09:12 +0000

    update api url

--- Proposed Solution.py
+++ Proposed Solution.py
@@ -94,4 +94,4 @@ if __name__ == '__main__':
         # Run if there are still frames left
         if (success):
             
-            # Apply background subtraction to extract foregrou
+            # Apply background subtraction to extract foreground (s
