421be7a2e96d06848f60397f46e7af713fcfdb49
qubilyan <151318276+qubilyan@users.noreply.github.com>
2021-12-30 07:36:52 +0000

    v1.1.6

--- Proposed Solution.py
+++ Proposed Solution.py
@@ -97,4 +97,4 @@ if __name__ == '__main__':
             # Apply background subtraction to extract foreground (silhouette)
             mask = makeSegMask(img)
             
-            # Apply thresholding to convert mask to 
+            # Apply thresholding to convert mask to binary ma
