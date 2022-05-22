a0db11c86b72fa0d9edb04dc88e64d367ba2acca
qubilyan <151318276+qubilyan@users.noreply.github.com>
2022-05-22 20:21:22 +0000

    share scheme

--- Proposed Solution.py
+++ Proposed Solution.py
@@ -109,4 +109,4 @@ if __name__ == '__main__':
             fps = str(fps)
             print(fps)
             # cv2.rectangle(mask, (10, 2), (100,20), (255,255,255), -1)
-            # cv2.putText(mask, fps, (15, 15), cv2.FO
+            # cv2.putText(mask, fps, (15, 15), cv2.FONT_HERSH
